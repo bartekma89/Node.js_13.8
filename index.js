1e35203ef1a6ac0de34e3b209988156bb4986470
Bartek <bartekma89@gmail.com>
2018-01-24 13:32:38 +0100

    uploaded static file from server

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
+var fs = require('fs');
 var http = require('http');
+var Color = require('colors');
+var path = require('path');
 
-var server = http.createServer();
+var server = http.createServer(function (req, res) {
 
-server.on('request', function(request, response) {
-    response.write('<body>')
-    response.write('Hello World!');
-    response.write('<h1>This is awsome!!!</h1>');
-    response.write('</body>')
-    response.end();
+    var file = null;
+    var fileName = null;
+
+    if (req.url === "/" || req.url === "/index.html") {
+        fileName = "index.html";
+        file = fs.createReadStream(path.join(__dirname, fileName));
+        res.writeHead(200, {
+            "Conteent-Type": "text/html"
+        });
+        file.pipe(res);
+    } else if (req.url === '/css/style.css') {
+        fileName = req.url.slice(1);
+        file = fs.createReadStream(path.join(__dirname, fileName));
+        res.writeHead(200, {
+            "Content-Type": "text/css"
+        });
+        file.pipe(res);
+    } else if (req.url === "/images/baseball.jpeg") {
+        fileName = req.url.slice(1);
+        file = fs.createReadStream(path.join(__dirname, fileName));
+        res.writeHead(200, {"Content-Type": "image/jpeg"});
+        file.pipe(res);
+    } else {
+        file = fs.createReadStream(path.join(__dirname, 'images', "404.jpeg"));
+        res.writeHead(404, {
+            "Content-Type": "image/jpeg"
+        });
+        file.pipe(res);
+    }
+
+
 });
 
-server.listen(9000);
+server.listen(8000, function () {
+    console.log('Serwer utworzony pod adresem http://localhost:8000'.green);
+})

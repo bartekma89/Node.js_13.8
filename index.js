0c47d2f34522b742199a90125f036ffec720ff99
Bartek <bartekma89@gmail.com>
2018-01-24 17:50:22 +0100

    refactored code

--- index.js
+++ index.js
@@ -13,18 +13,18 @@ var server = http.createServer(function (req, res) {
 
     if (req.url === "/" || req.url === "/index.html") {
         responseToClient('/index.html', 200);
-    } else if (req.url === '/css/style.css') {
-        responseToClient(req.url, 200);
-    } else if (req.url === "/images/friend.jpeg") {
-        responseToClient(req.url, 200);
-    } else {
+    } else if(req.statusCode === 404){
         responseToClient("/images/404.jpeg", 404)
+    } else {
+        responseToClient(req.url, 200);
     }
 
     function responseToClient(url, status) {
         var fileName = url.slice(1);
         var file = fs.createReadStream(path.join(__dirname, fileName));
-        res.writeHead(status, {"Content-Type": MIME_TYPES[path.extname(fileName)]});
+        res.writeHead(status, {
+            "Content-Type": MIME_TYPES[path.extname(fileName)]
+        });
         file.pipe(res);
     }
 
